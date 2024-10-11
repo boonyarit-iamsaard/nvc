cea8fb3665735b503a8cca45b8fd90925fd81c4a
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-10-11 22:53:20 +0700

    build: update root layout

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from 'react';
+
+import { SiteHeader } from '~/components/site-header';
+
+type PublicLayoutProps = {
+  children: ReactNode;
+};
+
+export default function PublicLayout({
+  children,
+}: Readonly<PublicLayoutProps>) {
+  return (
+    <>
+      <SiteHeader />
+      <main className="flex-1">{children}</main>
+    </>
+  );
+}
--- src/app/(public)/riva-waree-onsen/page.tsx
+++ src/app/(public)/riva-waree-onsen/page.tsx
@@ -1,9 +1,7 @@
 export default async function Page() {
   return (
-    <main className="flex-1">
-      <div className="flex h-80 flex-col items-center justify-center space-y-4 bg-muted text-muted-foreground">
-        <h1 className="font-serif text-4xl font-bold">Riva Waree Onsen</h1>
-      </div>
-    </main>
+    <div className="flex h-80 flex-col items-center justify-center space-y-4 bg-muted text-muted-foreground">
+      <h1 className="font-serif text-4xl font-bold">Riva Waree Onsen</h1>
+    </div>
   );
 }
