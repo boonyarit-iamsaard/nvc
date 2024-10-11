c3a8597c2eaf4b63b5e2603066e26b05bd99cfea
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-10-11 21:11:35 +0700

    build: add main navigation

---
+++ src/app/(public)/facilities/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+  return (
+    <main className="flex-1">
+      <div className="flex h-80 flex-col items-center justify-center space-y-4 bg-muted text-muted-foreground">
+        <h1 className="font-serif text-4xl font-bold">Facilities</h1>
+      </div>
+    </main>
+  );
+}
