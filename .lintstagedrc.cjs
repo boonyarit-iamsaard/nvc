0c314cf95cd2df2507ac00484708629a3016ef0e
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-10-11 12:17:32 +0700

    ci: add lint-staged

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,11 @@
+// See https://nextjs.org/docs/basic-features/eslint#lint-staged for details
+
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `next lint --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+
+module.exports = {
+  '*.{js,jsx,ts,tsx,cjs,mjs}': [buildEslintCommand, 'prettier --check .'],
+  '*': ['prettier --check --ignore-unknown'],
+};
