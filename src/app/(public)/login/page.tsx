69f07183ec78c8367c1fac7e57a9b36d3ae5a2c4
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-10-11 22:53:39 +0700

    feat(auth): add login page

---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '~/app/(public)/login/_components/login-form';
+
+export default function LoginPage() {
+  return (
+    <section className="container py-8">
+      <LoginForm />
+    </section>
+  );
+}
--- src/libs/auth/next-auth-provider.tsx
+++ src/libs/auth/next-auth-provider.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { type ReactNode } from 'react';
+import type { ReactNode } from 'react';
 
 import { SessionProvider } from 'next-auth/react';
 
-type NextAuthProviderProps = {
+type NextAuthProviderProps = Readonly<{
   children: ReactNode;
-};
+}>;
 export function NextAuthProvider({ children }: NextAuthProviderProps) {
   return (
     <SessionProvider refetchInterval={0} refetchOnWindowFocus={false}>
