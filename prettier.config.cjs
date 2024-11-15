3853a777532858ac7d5adbf41eb4c22bf3906618
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-11-15 16:29:07 +0700

    chore: add path alias for features

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -15,6 +15,7 @@ const config = {
     '^~/components/ui/(.*)$',
     '^~/configs/(.*)$',
     '^~/env',
+    '^~/features/(.*)$',
     '^~/libs/(.*)$',
     '^~/server/(.*)$',
     '^~/styles/(.*)$',
