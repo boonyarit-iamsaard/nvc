4e5f1a46fdcb31b44fb6f8f5ff69550976d8dd1a
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-11-30 16:56:46 +0700

    feat(auth): protect login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,19 @@
+import { redirect } from 'next/navigation';
+
+import { getServerAuthSession } from '~/core/auth/auth.config';
 import { LoginForm } from '~/core/auth/components/login-form';
 
-export default function Page() {
+export default async function Page() {
+  // TODO: reconsider protecte route with middleware
+  const session = await getServerAuthSession();
+
+  if (session) {
+    if (!session.user.firstLoginAt) {
+      redirect('/change-password');
+    }
+
+    redirect('/');
+  }
+
   return <LoginForm />;
 }
