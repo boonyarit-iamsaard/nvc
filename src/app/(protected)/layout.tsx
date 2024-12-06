92a04bbb040b8eba60f00747700e38459074c6dc
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-12-06 16:49:26 +0700

    refactor(auth): enhance protected layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,18 +1,58 @@
 import type { ReactNode } from 'react';
+import Link from 'next/link';
 
+import { ContentContainer } from '~/common/components/content-container';
+import { Message } from '~/common/components/message';
 import { SiteHeader } from '~/common/components/site-header';
+import { Button } from '~/common/components/ui/button';
+import { getServerAuthSession } from '~/core/auth/auth.config';
 
 type ProtectedLayoutProps = {
   children: ReactNode;
 };
 
-export default function ProtectedLayout({
+export default async function ProtectedLayout({
   children,
 }: Readonly<ProtectedLayoutProps>) {
+  const session = await getServerAuthSession();
+
   return (
     <>
       <SiteHeader />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1">
+        <div className="relative">
+          <div className="absolute inset-x-0 top-0 h-[35vh] bg-gradient-to-b from-muted/60 via-muted/30 to-background" />
+          <ContentContainer className="relative" layout="header" size="lg">
+            {session ? (
+              children
+            ) : (
+              <Message
+                variant="warning"
+                title="Access Restricted"
+                message="You need to be logged in to access this page"
+              >
+                <div className="grid gap-3 sm:grid-cols-6">
+                  <Button
+                    asChild
+                    size="sm"
+                    variant="outline"
+                    className="sm:col-span-2 sm:col-start-2"
+                  >
+                    <Link href="/">Return Home</Link>
+                  </Button>
+                  <Button
+                    asChild
+                    size="sm"
+                    className="sm:col-span-2 sm:col-start-4"
+                  >
+                    <Link href="/login">Login</Link>
+                  </Button>
+                </div>
+              </Message>
+            )}
+          </ContentContainer>
+        </div>
+      </main>
     </>
   );
 }
